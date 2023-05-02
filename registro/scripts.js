747596c0a51d48e49590e8db0ca6c501ba968627
Jonathan Gustavo de Oliveira Santos <130310262+JonathanGOSantos@users.noreply.github.com>
2023-05-02 17:15:37 -0300

    Projetos

---
+++ registro/scripts.js
@@ -0,0 +1,84 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordConfirmation = document.getElementById('password-confirmation')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const passwordConfirmationValue = passwordConfirmation.value;
+
+
+    // Validação de username
+    if (usernameValue === '') {
+        setErrorFor(username, 'O nome de usuário é obrigatório');
+    } else {
+        setSucessFor(username)
+    }
+
+    // Validação de email
+    if (email === '') {
+        setErrorFor(username, 'O email é obrigatório');
+    } else if(!checkEmail(emailValue)) {
+        setErrorFor(email, 'Por favor insira um email válido.');
+    } else {
+        setSucessFor(email)
+    }
+
+    // Validação de senha
+    if (passwordValue === '') {
+        setErrorFor(password, 'A senha é obrigatória');
+    } else if (passwordValue.length<8) {
+        setErrorFor(password, 'A senha precisa ter no mínimo 8 caracteres.');
+    } else {
+        setSucessFor(password)
+    }
+
+    // Validação de confirmação de senha
+    if (passwordConfirmationValue === '') {
+        setErrorFor(passwordConfirmation, 'A confirmação de senha é obrigatória');
+    } else if (passwordConfirmationValue != passwordValue) {
+        setErrorFor(passwordConfirmationValue, 'As senhas não conferem.');
+    } else {
+        setSucessFor(passwordConfirmation);
+    }
+
+    const formControls = form.querySelectorAll('.form-control')
+
+    const formIsValid = [... formControls].every(formControl => {
+        return (formControl.className === 'form-control success')
+    } )
+
+    if (formIsValid) {
+        alert('O Formulário é valido.')
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+    // Adicionar mensagem de erro
+    small.innerText = message;
+    // Adicionar classe de erro
+    formControl.className = "form-control error"
+}
+
+function setSucessFor(input) {
+    const formControl = input.parentElement;
+    // Adicionar classe de sucesso
+    formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
